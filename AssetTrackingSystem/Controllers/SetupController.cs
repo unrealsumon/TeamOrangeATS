2bb2fbe155a77367eed3568b0a826b112d6a2f70
sumon <sumon@DESKTOP-IT6B39G>
2017-05-05 02:22:14 +0600

    bootstrap modified.

---
+++ AssetTrackingSystem/Controllers/SetupController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AssetTrackingSystem.Controllers
+{
+    public class SetupController : Controller
+    {
+        // GET: Setup
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult SetupOrganization()
+        {
+            return View();
+        }
+
+    }
+}
